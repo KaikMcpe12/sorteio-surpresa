97ed40be3b373a6080cf24128abf2abe21818612
Kaik <119709479+KaikMcpe12@users.noreply.github.com>
2024-07-12 12:14:29 -0300

    :recycle: refactor:  changes to html files and features
    - new html files
    - changes to mechanics
    - bug fixes (message popping up ahead of time, wrong counter, clients WS server not running)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import http from 'http'
-import WebSocket from 'ws'
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const dotenv = require("dotenv");
 
 dotenv.config()
 
@@ -20,35 +20,71 @@ const ACTIONS = {
 }
 
 app.use('/public', express.static('public'))
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
-app.get('/admin', (req, res) => {
-    res.sendFile(__dirname + '/public/admin.html')
-})
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
+app.get('/client', (req, res) => res.sendFile(__dirname + '/public/client.html'))
+app.get('/admin', (req, res) => res.sendFile(__dirname + '/public/admin.html'))
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${URL}!`)
+server.listen(PORT, () => {
+    console.log(`Server is running on ${URL} !`)
 })
 
 let clients = []
 
-wss.on('connection', () => {
-    clients.push(wss)
-    updateAdminCount()
+wss.on('connection', (ws) => {
+    clients.push(ws)
+    updateAdminClientCount()
+    ws.on('close', () => {
+        clients = clients.filter((client) => client !== ws)
+        updateAdminClientCount()
+    })
+
+    ws.on('message', handleIncomingMessage.bind(null, ws))
 })
 
-function updateAdminCount(){
+function handleIncomingMessage(ws, msg) {
+    const data = JSON.parse(msg)
+    const action = data.action
+  
+    switch (action) {
+      case ACTIONS.ADMIN:
+        ws.isAdmin = true
+        updateAdminClientCount()//Limpar as constantes chamadas de funções
+        break;
+      case ACTIONS.DRAW:
+        handleDraw(data.code)
+        break;
+      default:
+        console.warn('Ação desconhecida:', action)
+    }
+}
+
+function handleDraw(confirmationCode){
+    let participants = Array.from(wss.clients).filter(
+      (client) => !client.isAdmin
+    )
+    const winner = participants[Math.floor(Math.random() * participants.length)]
+  
+    participants.forEach((client) => {
+      let result = JSON.stringify({ status: 'youlose' });
+      if (client === winner) {
+        result = JSON.stringify({ status: 'youwin', code: confirmationCode })
+      }
+      client.send(result)
+    })
+}
+
+function updateAdminClientCount(){
     const clientCount = Array.from(wss.clients).filter(
-        (client) => { !client.isAdmin}
+        (client) => !client.isAdmin 
     ).length
+    console.log(Array.from(wss.clients).length)
 
     Array.from(wss.clients).forEach((client) => {
         if(client.isAdmin && client.readyState === WebSocket.OPEN){
             client.send(
                 JSON.stringify({
                     action: ACTIONS.CLIENT_COUNT_UPDATE,
-                    count: clientCount
+                    count: clientCount,
                 })
             )
         }
